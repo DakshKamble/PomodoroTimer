c2e73d154a550fa53cd2fb7da165e678338b3a66
Daksh Kamble <63111617+DakshKamble@users.noreply.github.com>
2026-02-21 19:11:54 +0530

    Added Basic LED Comet
    Added Non blocking led comet animation

--- src/main.cpp
+++ src/main.cpp
@@ -2,24 +2,47 @@
 
 #include <FastLED.h>
 
-#define NUM_LEDS 12
-#define LED_PIN D7
+#define NUM_LEDS 12 // LED Count
+#define LED_PIN D7 // LED Pin
+#define LED_BRIGHTNESS 50
+#define ANIMATION_INTERVAL 50
 
-CRGB leds[NUM_LEDS];
+unsigned long lastUpdate = 0;
+CRGB leds[NUM_LEDS]; // We use this array to modify leds
+
+void fill_led();
 
 void setup() {
 
   Serial.begin(115200);
   Serial.println("Serial Debugging Started");
 
-  FastLED.addLeds<NEOPIXEL, LED_PIN>(leds, NUM_LEDS);
+  FastLED.addLeds<NEOPIXEL, LED_PIN>(leds, NUM_LEDS); // Setup LED Strip
+  FastLED.setBrightness(LED_BRIGHTNESS);
 
 }
 
 void loop() {
-  leds[0] = CRGB::Red;
-  FastLED.show();
-  delay(40);
 
+  unsigned long now = millis();
+
+  if(now - lastUpdate >= ANIMATION_INTERVAL) {
+    lastUpdate = now;
+    static int position = 0;
+
+    // Fade existing LEDs (creates trail)
+    fadeToBlackBy(leds, NUM_LEDS, 50);
+
+    // Light current position
+    leds[position] = CRGB::Red;
+
+    // Show update
+    FastLED.show();
+
+    // Move forward
+    position++;
+    if(position >= NUM_LEDS) {
+      position = 0;
+    }
+  }
 }
-
